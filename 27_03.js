8a2779444a1f4302a0c2296d97702b289316aabb
mila-droid7 <camilamendonca777@gmail.com>
2025-03-27 09:45:03 -0300

    Add files via upload

---
+++ 27_03.js
@@ -0,0 +1,11 @@
+const nome = prompt('qual é o seu nome?');
+
+alert('ola, ' + nome + "!");
+
+
+const numero1 = parseInt(prompt('me diga um numero'));
+const numero2 = parseInt(prompt('me diga outro numero'));
+
+const soma = numero1 + numero2;
+
+alert('a soma de ' + numero1 + " + " + numero2 + " é " + soma + "!");
